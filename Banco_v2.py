279ca67c1d98cb5b66f3a5676186dcd8a033e8cf
BregNights <daniel_baumann567@hotmail.com>
2024-05-28 17:47:09 -0300

    update 28_05

--- Banco_v2.py
+++ Banco_v2.py
@@ -10,44 +10,166 @@ Deve ser armazenado somentos os números do CPF. Não podemos cadastrar 2 usuár
 O numero da agência é fixo: "0001". O usuário pode ter mais de uma conta, mas a conta pertence somente a um usuário'''
 '''Dica: Para vincular um usuário a uma conta, filtre a lista de usuários buscando o número do CPF informado para cada usuário da lista'''
 
-import os
-os.system("cls")
+from os import system
+from time import sleep
+
+system("cls")
+
 def cadastro_cliente():
     cpf = input("Insira seu CPF: ").replace(".","").replace("-","")
-    checar_cpf(cpf)
+    filtrar_cpf(cpf)
 
 def criacao_cliente(nome_criacao, cpf_criacao):
-    global clientes
     clientes.append(nome_criacao)
     clientes.append(cpf_criacao)
-    criacao_conta()
     
 
-def checar_cpf(cpf_cliente):
+def filtrar_cpf(cpf_cliente):
     global clientes
-    for x in clientes:
-        if x == cpf_cliente:
+    for cpf in clientes:
+        if cpf == cpf_cliente:
             print("CPF em uso, utilize outro CPF ")
             cadastro_cliente()
     else:
         nome = input("Insira seu nome ")
         criacao_cliente(nome, cpf_cliente)
 
-def criacao_conta():
-    global conta
-    contador = 1
-    while conta[-1] > contador:
-    # conta[-1] = contador
-        contador +=1
-    conta.append(contador)
-    # conta.remove(0)
+# def criacao_conta():
+#     global conta
+#     contador = 1
+#     while conta[-1] > contador:
+#     # conta[-1] = contador
+#         contador +=1
+#     conta.append(contador)
+#     # conta.remove(0)
 
 
-clientes = []
-conta = [0]
+def depositar(saldo, deposito, extrato): #ok
+    if deposito > 0:
+        saldo += deposito
+        extrato += f"Depósito R${deposito:.2f}\n"
+        print(f"\nVocê depositou R${deposito:.2f}")
+        sleep(1)
+        return saldo, extrato
+  
+    else:
+        print("\nValor inválido")
+        sleep(2)
+    
+def sacar(saldo, saque, extrato, LIMITE_VALOR_SAQUE, quantidade_saque, LIMITE_SAQUE): #ok
+        saque_excedido = saque > saldo #Verifica se tem saldo para o saque
+        saque_limite = saque > LIMITE_VALOR_SAQUE #Verifica se o saque não ultrapassa o limite de valor por saque
+        limite_quantidade_saque = LIMITE_SAQUE == quantidade_saque #verifica se a qunatidade de saque é igual ao limite de saque
 
-cadastro_cliente()
-print(conta)
-cadastro_cliente()
-print (clientes)
-print (conta)
+        if saque_excedido:
+            print("\nSaldo para saque indisponível, verifique o valor em conta.")
+            sleep(2)
+
+        elif saque_limite:
+            print(f"\nValor ultrapassa o limite de R${LIMITE_VALOR_SAQUE:.2f} por saque.")
+            sleep(2)
+
+        elif limite_quantidade_saque:
+            print("\nQuantidade de saques diários excedido.")
+            sleep(2)
+
+        elif saque > 0:
+            saldo -= saque
+            LIMITE_SAQUE -= 1
+            extrato += f"Saque R${saque:.2f}\n"
+            print(f"\nVocê sacou: R${saque:.2f}")
+            sleep(1)
+
+        else:
+            print("\nValor inválido")
+            sleep(2)
+
+        return saldo, extrato
+
+def exibir_extrato(saldo, extrato):#ok
+    print(f"Valor em conta R${saldo:.2f}\n")
+    if not extrato:
+        print("Nenhum depósito ou saque")
+            
+    else:
+        print("Depósitos e saques\n")
+        print(extrato)
+        sleep(3)
+
+def menu(): #ok
+    menu = '''
+    Selecione uma operação para continuar
+    [1] Depósito
+    [2] Saque
+    [3] Extrato
+    [4] Nova Conta
+    [5] Contas Ativas
+    [6] Novo Usuário
+    [7] Listar Usuários
+    [0] Finalizar operações ou sair
+    Operação: '''
+    return int(input(menu))
+
+def main ():
+    AGENCIA = "0001"
+    LIMITE_SAQUE = 3
+    LIMITE_VALOR_SAQUE = 500
+
+    nome_banco = "Sem Nome"
+    saldo = 0
+    extrato = ""
+    quantidade_saque = 0
+
+    print(f'''
+      Bem vindo ao Banco {nome_banco}''')
+
+    while True:
+
+        operacao = menu()
+
+        if operacao == 1:
+            print("Você escolheu a operação de Depósito")
+            deposito = float(input("Digite o valor do depósito R$"))
+
+            saldo, extrato = depositar(saldo, deposito, extrato)
+
+        elif operacao == 2:
+            print("Você escolheu a operação Saque")
+            saque = float(input("Digite o valor do saque R$"))
+
+            saldo, extrato = sacar(
+                saldo=saldo,
+                saque=saque,
+                extrato=extrato,
+                LIMITE_VALOR_SAQUE=LIMITE_VALOR_SAQUE,
+                quantidade_saque=quantidade_saque,
+                LIMITE_SAQUE=LIMITE_SAQUE,
+            )
+        
+        elif operacao == 3:
+            print("Você escolheu a operação Extrato\n")
+
+            exibir_extrato(saldo, extrato)
+
+        elif operacao == 4:
+            pass
+
+        elif operacao == 5:
+            pass
+
+        elif operacao == 6:
+            pass
+
+        elif operacao == 7:
+            pass
+
+        elif operacao == 0:
+            print(f"\nFinalizando operações, o banco {nome_banco} agradece, tenha um ótimo dia")
+            sleep(2)
+            break
+
+        else:
+            print("\nOperação inexistente, selecione a opção corretamente")
+            sleep(2)
+
+main()
